6fa116de5153e9f4c97ce5fea8b5b870c89d942f
Fahim Faysal Siyam <fahim.siyam@upstra.com>
2024-04-28 15:20:36 +0600

    first

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
-import React, { Component } from 'react';
-import Stopwatch from './Stopwatch';
-import './App.css';
+import React, { Component } from "react";
+import Stopwatch from "./Stopwatch";
+import "./App.css";
 
 class App extends Component {
   render() {
     return (
-      <div className='App-Container'>
-        <div className='App'>
+      <div className="App-Container">
+        <div className="App">
+          <h1 style={{ color: "white", fontSize: "100px" }}>
+            Fahim Faysal Siyam{" "}
+          </h1>
           <Stopwatch />
         </div>
       </div>
--- src/Stopwatch.js
+++ src/Stopwatch.js
@@ -1,28 +1,34 @@
-import React, { Component } from 'react';
-import { Form, FormGroup, ControlLabel, Button, ButtonGroup } from 'react-bootstrap';
-import './Stopwatch.css';
+import React, { Component } from "react";
+import {
+  Form,
+  FormGroup,
+  ControlLabel,
+  Button,
+  ButtonGroup,
+} from "react-bootstrap";
+import "./Stopwatch.css";
 
 const Separator = () => {
-  return <ControlLabel className='Stopwatch-number'>:</ControlLabel>;
-}
+  return <ControlLabel className="Stopwatch-number">:</ControlLabel>;
+};
 
 class Stopwatch extends Component {
   constructor(props) {
     super(props);
     this.state = {
       tick: null,
-      totalSeconds: 0
+      totalSeconds: 0,
     };
   }
 
   incrementCount() {
     this.setState({
-      totalSeconds: this.state.totalSeconds + 1
+      totalSeconds: this.state.totalSeconds + 1,
     });
   }
 
   getHours() {
-    return parseInt(this.state.totalSeconds / 60 / 60 ) % 24;
+    return parseInt(this.state.totalSeconds / 60 / 60) % 24;
   }
 
   getMinutes() {
@@ -36,98 +42,130 @@ class Stopwatch extends Component {
   startCounter = () => {
     clearInterval(this.state.tick);
     this.setState({
-      tick: setInterval(() => this.incrementCount(), 1000)
+      tick: setInterval(() => this.incrementCount(), 1000),
     });
-  }
+  };
 
   stopCounter = () => {
     clearInterval(this.state.tick);
     this.setState({
-      tick: null
+      tick: null,
     });
-  }
+  };
 
   resetCounter = () => {
     clearInterval(this.state.tick);
     this.setState({
       totalSeconds: 0,
-      tick: null
+      tick: null,
     });
-  }
+  };
 
   resumeCounter = () => {
     clearInterval(this.state.tick);
     this.setState({
-      tick: setInterval(() => this.incrementCount(), 1000)
+      tick: setInterval(() => this.incrementCount(), 1000),
     });
-  }
+  };
 
   render() {
     let buttons = null;
-    let started = this.getHours() > 0 || this.getMinutes() > 0 || this.getSeconds() > 0;
+    let started =
+      this.getHours() > 0 || this.getMinutes() > 0 || this.getSeconds() > 0;
     if (!this.state.tick && !started) {
       buttons = (
-          <ButtonGroup justified>
-            <ButtonGroup className='Stopwatch-button-group'>
-              <Button bsStyle='info' bsSize='large' block
-                className='Stopwatch-button'
-                onClick={this.startCounter}>Start</Button>
-            </ButtonGroup>
+        <ButtonGroup justified>
+          <ButtonGroup className="Stopwatch-button-group">
+            <Button
+              bsStyle="info"
+              bsSize="large"
+              block
+              className="Stopwatch-button"
+              onClick={this.startCounter}
+            >
+              Start
+            </Button>
           </ButtonGroup>
-        );
+        </ButtonGroup>
+      );
     } else if (!this.state.tick && started) {
       buttons = (
-          <ButtonGroup justified>
-            <ButtonGroup className='Stopwatch-button-group'>
-              <Button bsStyle='info' bsSize='large' block
-                className='Stopwatch-button'
-                onClick={this.resumeCounter}>Resume</Button>
-            </ButtonGroup>
-            <ButtonGroup className='Stopwatch-button-group'>
-                <Button bsSize='large' block
-                  className='Stopwatch-button'
-                  onClick={this.resetCounter}>Reset</Button>
-              </ButtonGroup>
+        <ButtonGroup justified>
+          <ButtonGroup className="Stopwatch-button-group">
+            <Button
+              bsStyle="info"
+              bsSize="large"
+              block
+              className="Stopwatch-button"
+              onClick={this.resumeCounter}
+            >
+              Resume
+            </Button>
           </ButtonGroup>
-        );
+          <ButtonGroup className="Stopwatch-button-group">
+            <Button
+              bsSize="large"
+              block
+              className="Stopwatch-button"
+              onClick={this.resetCounter}
+            >
+              Reset
+            </Button>
+          </ButtonGroup>
+        </ButtonGroup>
+      );
     } else {
-        buttons = (
-          <ButtonGroup justified>
-            <ButtonGroup className='Stopwatch-button-group'>
-              <Button bsStyle='danger' bsSize='large' block
-                className='Stopwatch-button'
-                onClick={this.stopCounter}>Stop</Button>
-            </ButtonGroup>
-            <ButtonGroup className='Stopwatch-button-group'>
-                <Button bsSize='large' block
-                  className='Stopwatch-button'
-                  onClick={this.resetCounter}>Reset</Button>
-              </ButtonGroup>
+      buttons = (
+        <ButtonGroup justified>
+          <ButtonGroup className="Stopwatch-button-group">
+            <Button
+              bsStyle="danger"
+              bsSize="large"
+              block
+              className="Stopwatch-button"
+              onClick={this.stopCounter}
+            >
+              Stop
+            </Button>
           </ButtonGroup>
-        );
+          <ButtonGroup className="Stopwatch-button-group">
+            <Button
+              bsSize="large"
+              block
+              className="Stopwatch-button"
+              onClick={this.resetCounter}
+            >
+              Reset
+            </Button>
+          </ButtonGroup>
+        </ButtonGroup>
+      );
     }
     return (
-      <div className='Stopwatch'>
-          <Form className='Stopwatch-display'>
-            <FormGroup bsSize='large' controlId='formStopwatch'>
-              <ControlLabel className='Stopwatch-number'>{this.leadingZero(this.getHours())}</ControlLabel>
-              <Separator />
-              <ControlLabel className='Stopwatch-number'>{this.leadingZero(this.getMinutes())}</ControlLabel>
-              <Separator />
-              <ControlLabel className='Stopwatch-number'>{this.leadingZero(this.getSeconds())}</ControlLabel>
-            </FormGroup>
-            <FormGroup>
-              {buttons}
-            </FormGroup>
-          </Form>
+      <div className="Stopwatch">
+        <Form className="Stopwatch-display">
+          <FormGroup bsSize="large" controlId="formStopwatch">
+            <ControlLabel className="Stopwatch-number">
+              {this.leadingZero(this.getHours())}
+            </ControlLabel>
+            <Separator />
+            <ControlLabel className="Stopwatch-number">
+              {this.leadingZero(this.getMinutes())}
+            </ControlLabel>
+            <Separator />
+            <ControlLabel className="Stopwatch-number">
+              {this.leadingZero(this.getSeconds())}
+            </ControlLabel>
+          </FormGroup>
+          <FormGroup>{buttons}</FormGroup>
+        </Form>
       </div>
     );
   }
 
   leadingZero(num) {
-    return num < 10 ? '0' + num : num;
+    return num < 10 ? "0" + num : num;
   }
 }
 
 export default Stopwatch;
-
